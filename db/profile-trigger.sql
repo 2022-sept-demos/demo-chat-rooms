d6cf7f56cc75611b4a0a18cb6c976c8938b60a2e
Marty Nelson <marty@martypdx.com>
2022-10-11 08:36:48 -0700

    include profile trigger

---
+++ db/profile-trigger.sql
@@ -0,0 +1,20 @@
+drop trigger if exists on_auth_user_created on auth.users;
+drop function if exists handle_new_user;
+
+-- inserts a row into public.users
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+  insert into public.profiles (id, username)
+  values (new.id, split_part(new.email, '@', 1));
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
